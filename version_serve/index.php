889730b7966cf1a7d86332fd683d5136bfef2b54
Daaron Dwyer <daarond@gmail.com>
2018-02-15 16:24:19 -0600

    still trying to solve the updater

--- version_serve/index.php
+++ version_serve/index.php
@@ -1,8 +1,10 @@
 <?php
 if ($_REQUEST["req"]==='ios') {
+	header('content-type: application/zip');
 	readfile('ios.zip');
 }
 else if ($_REQUEST["req"]==='android') {
+	header('content-type: application/zip');
 	readfile('android.zip');
 }
 else {
